ba6b6dc7ea09ffbcc1e3c2d3e4205eb9800f4e05
DB3CH <48409931+DB3CH@users.noreply.github.com>
2020-02-25 01:02:03 +0100

    nav bar

--- index.php
+++ index.php
@@ -3,9 +3,18 @@
 <head>
 	<title>Støyer</title>
 	<meta charset="utf-8">
-	
+	<link rel="stylesheet" type="text/css" href="strøyercss.css">
 </head>
-<body>
 
+<body>
+	<div >
+		<ul> 
+			<li class="navigationsbar"><a href="Webshop.php">Webshop</a></li>
+			<li class="navigationsbar"><a href="om_os.php">Om os</a></li>
+			<li class="navigationsbar"><a href="kontakt.php">Kontakt</a></li>
+
+		</ul>	
+	
+	</div>
 </body>
 </html>
