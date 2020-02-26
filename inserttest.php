4d52544ef35a7af1e2f29d95f8c9c21842b3d91f
DB3CH <48409931+DB3CH@users.noreply.github.com>
2020-02-26 11:41:55 +0100

    opdatering af databasen

---
+++ inserttest.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Insert Test </title>
+	<meta charset="utf-8">
+</head>
+
+<body>
+	<form action="phpaction.php" method="POST">
+        <label>Navn:</label>
+        <input type="text" id="navn" name="navn">
+        <Lable>Efternavn:</Lable>
+        <input type="text" id="efternavn" name="efternavn"> 
+        <input type="submit" value="Indsæt">
+    </form>
+</body>
+</html>
---
+++ phpaction.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+$username = "admin1";
+$password = "admin1";
+$dbname = "test";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+  $navn = htmlentities($_POST['navn']);
+  $efternavn = htmlentities($_POST['efternavn']);
+  
+
+$sql = "INSERT INTO testtable (navn, efternavn)
+VALUES ('$navn', '$efternavn')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
