b58249bef204faf85e4d936c8ad94a9dfd0f1e7c
DB3CH <48409931+DB3CH@users.noreply.github.com>
2020-02-26 12:04:36 +0100

    update

--- phpaction.php
+++ phpaction.php
@@ -21,9 +21,14 @@ VALUES ('$navn', '$efternavn')";
 
 if (mysqli_query($conn, $sql)) {
     echo "New record created successfully";
+    Sleep(5);
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
+
+
+
+
 
 mysqli_close($conn);
 ?>
