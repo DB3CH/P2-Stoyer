fce5c7e140b7f6ac4069c50ef02c914d0819965b
DB3CH <48409931+DB3CH@users.noreply.github.com>
2020-03-04 12:34:18 +0100

    katagori

--- update.php
+++ update.php
@@ -2,7 +2,7 @@
 // Etablerer forbindelse til serveren
 require_once "connection.php";
 
-//Tager informationen fra formen og sætter den i variabler, HTMLentities sørger for at det ikke er skrevet kode i formen. 
+//Tager informationen fra formen og sætter den i variabler, HTMLentities sørger for at det ikke er skrevet kode i formen.
   $kategori = ($_POST['kategori']);
   $producent = ($_POST['producent']);
   $model = htmlentities($_POST['model']);
@@ -11,9 +11,9 @@ require_once "connection.php";
   $id = htmlentities($_POST['id']);
 
 
-  
+
 //Sætter sql query til at indsætte data i en variabel
-$sql = "UPDATE produkttest SET katagori='$katagori', producent='$producent', model='$model', pris='$pris', beskrivelse='$beskrivelse' 
+$sql = "UPDATE produkttest SET kategori='$kategori', producent='$producent', model='$model', pris='$pris', beskrivelse='$beskrivelse'
 WHERE id='$id';";
 
 if (mysqli_query($connection, $sql)) {
@@ -24,7 +24,7 @@ if (mysqli_query($connection, $sql)) {
 
 
 
-//header sender brugeren videre til show.php 
+//header sender brugeren videre til show.php
 header("location: show.php");
 
 
