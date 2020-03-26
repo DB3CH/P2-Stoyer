fd6a4bcf2a48a9ed2006dcb4b3256780995ce5ff
DB3CH <48409931+DB3CH@users.noreply.github.com>
2020-03-26 11:39:18 +0100

    kode

--- phptilfoejadmin.php
+++ phptilfoejadmin.php
@@ -2,13 +2,13 @@
 // Etablerer forbindelse til serveren
 require_once "connection.php";
 
-//Tager informationen fra formen og sætter den i variabler, HTMLentities sørger for at det ikke er skrevet kode i formen. 
+//Tager informationen fra formen og sætter den i variabler, HTMLentities sørger for at det ikke er skrevet kode i formen.
   $username = ($_POST['username']);
   $kode = ($_POST['kode']);
 
-  
+
 //Sætter sql query til at indsætte data i en variabel
-$sql = "INSERT INTO users (username, kode)
+$sql = "INSERT INTO users (username, password)
 VALUES ('$username', '$kode')";
 
 //if staement viser om dataen er skrevet eller om der er en error
@@ -18,7 +18,7 @@ if (mysqli_query($connection, $sql)) {
     echo "Error: " . $sql . "<br>" . mysqli_error($connection);
 }
 
-//header sender brugeren videre til show.php 
+//header sender brugeren videre til show.php
 header("location: show.php");
 
 
