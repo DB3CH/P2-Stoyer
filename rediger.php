b9d251802e8e40161f5c9674a19df9c999208d3e
DB3CH <48409931+DB3CH@users.noreply.github.com>
2020-04-06 00:25:39 +0200

    Sammensætning af filer

--- rediger.php
+++ rediger.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'connection.php';
 session_start();
 require_once "connection.php";
 
@@ -7,12 +8,39 @@ if(!isset($_SESSION['login'])){
     header('Location: login.php');
 }
 
+//Kode til at redigere i tøjet, bliver kun kørt hvis der trykkes på rediger knappen
+if (isset($_POST['rediger'])) {
+      //Tager informationen fra formen og sætter den i variabler, HTMLentities sørger for at det ikke er skrevet kode i formen.
+        $kategori = ($_POST['kategori']);
+        $producent = ($_POST['producent']);
+        $model = htmlentities($_POST['model']);
+        $beskrivelse = ($_POST['beskrivelse']);
+        $pris = htmlentities($_POST['pris']);
+        $id = htmlentities($_POST['id']);
+
+
+
+      //Sætter sql query til at indsætte data i en variabel
+      $sql = "UPDATE produkttest SET kategori='$kategori', producent='$producent', model='$model', pris='$pris', beskrivelse='$beskrivelse'
+      WHERE id='$id';";
+
+      if (mysqli_query($connection, $sql)) {
+          echo "New record created successfully";
+      } else {
+          echo "Error: " . $sql . "<br>" . mysqli_error($connection);
+      }
+
+
+      //header sender brugeren videre til show.php
+      header("location: show.php");
+}
+
 
 ?>
 
 
 <?php
-require_once 'connection.php';
+
 
 if(isset($_GET['id'])){
 	//id fra URL bliver sat i en variabel
@@ -38,9 +66,9 @@ if(isset($_GET['id'])){
 	<meta charset="utf-8">
 </head>
 
-<body> 
+<body>
 
-	<form name="rediger" id="rediger" action="update.php" method="POST" autocomplete="on">
+	<form name="rediger" id="rediger" action="<?php echo $_SERVER['PHP_SELF']?>" method="POST" autocomplete="on">
         <label>Kategori:</label>
         <input type="text" id="kategori" name="kategori" value="<?php echo $row['kategori']?>">
         <Lable>Producent:</Lable>
@@ -52,7 +80,7 @@ if(isset($_GET['id'])){
         <Lable>Pris:</Lable>
         <input type="number" min="0.00" max="999999.00" step="1.00" id="pris" name="pris" value="<?php echo $row['pris']?>">
 
-        <input type="submit" value="Rediger">
+        <input type="submit" name="rediger" value="Rediger">
         <input type="hidden" name="id" value="<?php echo $id?>">
     </form>
 
