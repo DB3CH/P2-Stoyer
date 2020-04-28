32642e5cc717f5abe6508c6f5e8a635bd724eae4
DB3CH <48409931+DB3CH@users.noreply.github.com>
2020-04-28 12:05:04 +0200

    Update info.php

--- info.php
+++ info.php
@@ -13,12 +13,22 @@
       Leverings oplysninger:
       <br>
       Navn:<input type="text" name="navn" placeholder="Indtast navn" required>
-      Efternavn:
-      Adresse:
-      postnummer:
-      By:
-      Land:
-      Telefonnummer:
+      <br>
+      Efternavn: <input type="text" name="efternavn" placeholder="Indtast efternavn" required>
+      <br>
+      Adresse: <input type="text" name="adresse" placeholder="Indtast adresse" required>
+      <br>
+      postnummer: <input type="number" maxlength="4" name="postnummer" placeholder="Postnummer" required>
+      <br>
+      By: <input type="text" name="by" placeholder="By" required>
+      <br>
+      Land: <input type="text" name="land" placeholder="Land" required>
+      <br>
+      Telefonnummer: <input type="number" name="telefonnummer" maxlength="8" placeholder="Telefonnummer" equired>
+      <br>
+      <input type="submit" name="submit" value="Til Betaling">
+
+
     </form>
   </body>
 </html>
