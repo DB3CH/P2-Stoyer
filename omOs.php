d7815be733880e0aafaccd81400a114d207cac91
rasm802a <48472560+rasm802a@users.noreply.github.com>
2020-04-29 14:34:03 +0200

    Fonts, kurv, og gennerel styling

--- omOs.php
+++ omOs.php
@@ -3,17 +3,17 @@
 <head>
 	<title>Product</title>
 	<link rel="stylesheet" href="style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
   	<meta charset="utf-8">
 </head>
 <body>
-
-	<div class="shoppingcart">
+	<div class="cart">
 	<a href="viskurv.php">
-		<img class="shoppingcart" src="shoppingcart.png">
+		<img class="cart" src="cart.png">
 		<p>0,00 kr.</p>
 	</a>
 	</div>
-	
+
 	<div class="pageNav">
 
 		<label for="toggle">&#9776;</label>
@@ -21,7 +21,7 @@
 
 
 		<div class="pageHeader">
-		<a href="index.php"><h1>STRØYER</h1></a>
+		<a href="forside.php"><h1>STRØYER</h1></a>
 		</div>
 
 		<ul class="pageMenu">
